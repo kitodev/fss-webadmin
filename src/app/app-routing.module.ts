fae0d47424d44b40cb9afa14435d20147a0f1953
kitodev <tkornel19@gmail.com>
2023-02-21 22:51:20 +0000

    material added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-
+  {path: '/login', component: LoginComponent},
+  {path: '/create-registration', component: CreateRegistrationComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
